a8ec6d92a48b4be1fe43567d150271eb376470d0
Joon Baek <joonbaek12@gmail.com>
2024-02-07 10:55:11 +0900

    [Setting] #1 - 프로젝트 초기 셋팅

---
+++ EasyWeather-iOS/Presentation/Main/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyWeather-iOS
+//
+//  Created by Joon Baek on 2024/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .mainTheme
+    }
+
+
+}
+
