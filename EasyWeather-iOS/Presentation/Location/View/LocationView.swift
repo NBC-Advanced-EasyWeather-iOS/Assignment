3492c103e0caa23c28e8167ab1a149638f013da0
codeIsGone <ghdgmlrhs@gmail.com>
2024-02-08 15:31:21 +0900

    [feat] #12 - 작업 초기 세팅, titleLabel 구현 완료

---
+++ EasyWeather-iOS/Presentation/Location/View/LocationView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationView.swift
+//  EasyWeather-iOS
+//
+//  Created by 홍희곤 on 2/8/24.
+//
+
+import UIKit
+
+class LocationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EasyWeather-iOS/Presentation/Location/ViewController/LocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationViewController.swift
+//  EasyWeather-iOS
+//
+//  Created by 홍희곤 on 2/8/24.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
