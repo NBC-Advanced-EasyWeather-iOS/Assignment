caef33b02cd7e72e4526669ded3863749bb25e85
yenny <yennydev42@gmail.com>
2024-02-08 09:24:46 +0900

    [Feat] #3 - 메인화면 VC 구현

--- EasyWeather-iOS/Presentation/Main/ViewController/ViewController.swift
+++ EasyWeather-iOS/Presentation/Main/ViewController/ViewController.swift
@@ -7,14 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import SnapKit
 
+class ViewController: UIViewController {
+    
+    // MARK: - UI Properties
+    
+    private var pagingControlView: PagingControlView!
+
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .mainTheme
+        pagingControlView = PagingControlView(numberOfPages: 3)
+        
+        setUI()
+        setLayout()
     }
-
 
 }
 
+// MARK: - Extensions
+
+extension ViewController {
+    private func setUI() {
+        view.backgroundColor = .primaryBackground
+        self.navigationController?.isNavigationBarHidden = true
+        
+        [pagingControlView].forEach {
+            view.addSubview($0)
+        }
+    }
+
+    private func setLayout() {
+        pagingControlView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
