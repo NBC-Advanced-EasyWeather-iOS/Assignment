76adab70a45dab0c4d8fe5b4f630bb0160ab3457
yenny <yennydev42@gmail.com>
2024-02-13 11:54:26 +0900

    [Feat] #19 - 위치정보접근권한 요청 추가

--- EasyWeather-iOS/Presentation/Main/ViewController/ViewController.swift
+++ EasyWeather-iOS/Presentation/Main/ViewController/ViewController.swift
@@ -6,24 +6,53 @@
 //
 
 import UIKit
-
+import CoreLocation
 import SnapKit
 
 final class ViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    private var locationManager: CLLocationManager!
+    
     // MARK: - UI Properties
     
     private var pagingControlView: PagingControlView!
-
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
         pagingControlView = PagingControlView(numberOfPages: 3)
         
         setUI()
         setLayout()
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+    // 위치 권한 상태 변경시 호출
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            // MARK: 위치 권한을 요청하지 않은 경우 - 권한 요청
+            manager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse, .authorizedAlways:
+            // MARK: 위치 권한을 얻은 경우
+            print("Location access granted")
+        case .denied, .restricted:
+            // MARK: 위치 권한을 얻지 못한 경우 - 위치 검색 페이지로 이동
+            print("Location access denied")
+//            self.navigationController?.present(<#T##viewControllerToPresent: UIViewController##UIViewController#>, animated: true)
+        @unknown default:
+            fatalError("Unknown location authorization status")
+        }
     }
 }
 
