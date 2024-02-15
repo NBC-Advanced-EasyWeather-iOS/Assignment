c8c1b725ec23be529c545757d91509d8a52db621
lus0401 <lus0401@gmail.com>
2024-02-15 12:55:43 +0900

    [Chore] #29 - 위클리 페이지 상단 이미지 뷰 구현

--- EasyWeather-iOS/Presentation/WeeklyWeather/View/WeeklyWeatherView.swift
+++ EasyWeather-iOS/Presentation/WeeklyWeather/View/WeeklyWeatherView.swift
@@ -10,38 +10,61 @@ import UIKit
 import SnapKit
 
 class WeeklyWeatherView: UIView {
-    let imageView = UIImageView()
+    let stackView = UIStackView()
+    let leftSpacerView = UIView()
+    let weeklyWeatherImageView = UIImageView()
     let tableView: UITableView = UITableView(frame: .zero, style: .insetGrouped)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupViews()
     }
-
+    
     private func setupViews() {
-        // 이미지 뷰 설정
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        imageView.backgroundColor = .lightGray
-        addSubview(imageView)
-
+        addSubview(stackView)
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.backgroundColor = UIColor.lightTheme
+        
+        stackView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.3)
+        }
+        
+        stackView.addArrangedSubview(leftSpacerView)
+        stackView.addArrangedSubview(weeklyWeatherImageView)
+        
+        weeklyWeatherImageView.contentMode = .scaleAspectFit
+        weeklyWeatherImageView.clipsToBounds = true
+        
+        if let image = UIImage(named: "DayCloud") {
+            weeklyWeatherImageView.image = image
+        } else {
+            // 이미지를 찾을 수 없을 경우의 처리
+            weeklyWeatherImageView.backgroundColor = UIColor.lightTheme
+        }
+        
         // 테이블 뷰 설정
         tableView.register(WeeklyTableViewCell.self, forCellReuseIdentifier: "cell")
         addSubview(tableView)
-
+        
         // SnapKit을 사용하여 오토레이아웃 설정
-        imageView.snp.makeConstraints { make in
-            make.top.leading.trailing.equalToSuperview()
-            make.height.equalToSuperview().multipliedBy(0.30)
+        leftSpacerView.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(3) // 왼쪽 공간은 스택뷰의 1/3 크기로 설정
         }
-
+        
+        weeklyWeatherImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(stackView.snp.height)
+        }
+        
         tableView.snp.makeConstraints { make in
-            make.top.equalTo(imageView.snp.bottom)
+            make.top.equalTo(weeklyWeatherImageView.snp.bottom)
             make.leading.trailing.bottom.equalToSuperview()
         }
     }
