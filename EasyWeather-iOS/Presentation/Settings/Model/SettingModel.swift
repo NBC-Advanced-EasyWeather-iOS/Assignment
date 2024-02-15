26d57395dbbd2b6ed0440c7acb43d649848860ae
yenny <yennydev42@gmail.com>
2024-02-15 15:58:50 +0900

    [Feat] #27 - 세팅 UserDefault 싱글톤

--- EasyWeather-iOS/Presentation/Settings/Model/SettingModel.swift
+++ EasyWeather-iOS/Presentation/Settings/Model/SettingModel.swift
@@ -1,5 +1,5 @@
 
-// MARK: - SettingOption Model
+import Foundation
 
 class SettingOptionModel {
     let title: String
@@ -10,3 +10,32 @@ class SettingOptionModel {
         self.isOn = isOn
     }
 }
+
+class SettingOptionUserDefault {
+    static let shared = SettingOptionUserDefault()
+    
+    private init() {}
+    
+    let optionKeys: [String] = [
+        "일출/일몰 시간",
+        "최저/최고 기온",
+        "기압",
+        "습도",
+        "섭씨온도 °C",
+        "화씨온도 °F"
+    ]
+    
+    let settingOptions: [String] = ["일출","일몰","최저 기온","최고 기온","기압","습도"]
+    var settingOptionsData: [SettingOptionModel] = []
+    
+    func loadOptionsFromUserDefaults() -> [SettingOptionModel] {
+        optionKeys.forEach { option in
+            if let value = UserDefaults.standard.string(forKey: option) {
+                let boolValue = value == "1"
+                settingOptionsData.append(SettingOptionModel(title: option, isOn: boolValue))
+            }
+        }
+        return settingOptionsData
+    }
+}
+
